1e4751c25b1f7afd5a5f3e0da84f9572693f14dc
Juan Luis Font <j.l.font@amc.uva.nl>
2014-07-15 15:22:19 +0200

    including license for pythong and bash scripts

--- nsg-tools/nsg-debugging-tools/external/lib/python/elementtree/HTMLTreeBuilder.py
+++ nsg-tools/nsg-debugging-tools/external/lib/python/elementtree/HTMLTreeBuilder.py
@@ -1,3 +1,20 @@
+"""
+Copyright (C) 2013 Academic Medical Center of the University of Amsterdam
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or 
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+ 
+
 #
 # ElementTree
 # $Id: HTMLTreeBuilder.py 2325 2005-03-16 15:50:43Z fredrik $
